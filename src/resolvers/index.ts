55c22b0559f728f7a951c34915fa76911fe746d2
Johannes Schickling <schickling.j@gmail.com>
2018-01-03 13:19:09 +0100

    Added subscriptions support

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,11 @@
 import { extractFragmentReplacements } from 'graphcool-binding'
+import { Query } from './Query'
+import { Subscription } from './Subscription'
 import { auth } from './Mutation/auth'
 import { Home } from './Home'
 import { ExperiencesByCity } from './ExperiencesByCity'
 import { Viewer } from './Viewer'
 import { AuthPayload } from './AuthPayload'
-import { Query } from './Query'
 import { book } from './Mutation/book'
 import { addPaymentMethod } from './Mutation/addPaymentMethod'
 
@@ -15,6 +16,7 @@ export const resolvers = {
     book,
     addPaymentMethod,
   },
+  Subscription,
   Viewer,
   ExperiencesByCity,
   Home,
