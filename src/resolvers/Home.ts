8f77f38fec88529ec72606c4116b0eee851d5090
c-riq <chrisrieck6@gmail.com>
2018-01-06 16:03:20 +0100

    add more seed data, add price filter

--- src/resolvers/Home.ts
+++ src/resolvers/Home.ts
@@ -8,4 +8,11 @@ export const Home = {
       return places.length
     },
   },
+  perNight: {
+    fragment: `fragment NumRatings on Place { id }`,
+   	resolve: async ({ id }, args, ctx: Context, info) => {
+      const place = await ctx.db.query.place({ where: { id } },`{ pricing { perNight } }`)
+      return place.pricing.perNight
+    },
+},
 }
