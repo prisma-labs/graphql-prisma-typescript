e8d4a0d1e2120b82b7fa0ab30d8ca3a3c40a2e62
Johannes Schickling <schickling.j@gmail.com>
2018-02-27 11:45:47 +0100

    Implemented Home.avgRating

--- src/resolvers/Home.ts
+++ src/resolvers/Home.ts
@@ -1,6 +1,7 @@
 import { Context } from '../utils'
 
 export const Home = {
+  // TODO rewrite this once this lands: https://github.com/graphcool/prisma/issues/1312
   numRatings: {
     fragment: `fragment NumRatings on Place { id }`,
     resolve: async ({ id }, args, ctx: Context, info) => {
@@ -13,13 +14,20 @@ export const Home = {
   },
 
   perNight: {
-    fragment: `fragment PerNight on Place { id }`,
-    resolve: async ({ id }, args, ctx: Context, info) => {
-      const place = await ctx.db.query.place(
-        { where: { id } },
-        `{ pricing { perNight } }`,
-      )
-      return place.pricing.perNight
+    fragment: `fragment PerNight on Place { pricing { perNight } }`,
+    resolve: ({ pricing: { perNight } }) => perNight,
+  },
+
+  // TODO rewrite this once this lands: https://github.com/graphcool/prisma/issues/1312
+  avgRating: {
+    fragment: `fragment AvgRating on Place { reviews { stars } }`,
+    resolve: ({ reviews }) => {
+      if (reviews.length > 0) {
+        return (
+          reviews.reduce((acc, { stars }) => acc + stars, 0) / reviews.length
+        )
+      }
+      return null
     },
   },
 }
