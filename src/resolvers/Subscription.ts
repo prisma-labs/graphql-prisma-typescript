319c9afd67286f0ad7bce7c2a1827d38240b096e
timsuchanek <tim.suchanek@gmail.com>
2018-05-10 14:06:07 +0200

    fix subscriptions, add wrapquery example

--- src/resolvers/Subscription.ts
+++ src/resolvers/Subscription.ts
@@ -1,30 +1,31 @@
 import { Context } from '../utils'
-import { WrapQuery } from 'graphql-tools'
-import { SelectionSetNode, Kind } from 'graphql'
+// import { WrapQuery } from 'graphql-tools'
+// import { SelectionSetNode, Kind } from 'graphql'
 
 export const Subscription = {
   city: {
     subscribe: async (parent, args, ctx: Context, info) => {
       return ctx.db.subscription.city({}, info, {
-        transforms: [
-          new WrapQuery(
-            ['city'],
-            (subtree: SelectionSetNode) => ({
-              kind: Kind.FIELD,
-              name: {
-                kind: Kind.NAME,
-                value: 'node',
-              },
-              selectionSet: subtree,
-            }),
-            // result => result && result.node,
-            result => {
-              // TODO clean me up
-              console.log({ result })
-              return result && result.node
-            },
-          ),
-        ],
+        // TODO reenable when https://github.com/apollographql/graphql-tools/issues/780 is solved
+        // transforms: [
+        //   new WrapQuery(
+        //     ['city'],
+        //     (subtree: SelectionSetNode) => ({
+        //       kind: Kind.FIELD,
+        //       name: {
+        //         kind: Kind.NAME,
+        //         value: 'node',
+        //       },
+        //       selectionSet: subtree,
+        //     }),
+        //     // result => result && result.node,
+        //     result => {
+        //       // TODO clean me up
+        //       console.log({ result })
+        //       return result && result.node
+        //     },
+        //   ),
+        // ],
       })
     },
   },
