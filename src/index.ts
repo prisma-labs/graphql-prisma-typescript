1befbbd296ebc06ed585bc7eb206961796fa0d02
schickling <schickling.j@gmail.com>
2018-10-20 15:33:34 +0200

    Added stricter TypeScript checking

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { Prisma } from './generated/prisma-client'
 import { resolvers } from './resolvers'
 
 const db = new Prisma({
-  endpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_SECRET,
+  endpoint: process.env.PRISMA_ENDPOINT!,
+  secret: process.env.PRISMA_SECRET!,
   // debug: true,
 })
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers,
+  resolvers: resolvers as any,
   context: req => ({ ...req, db }),
-} as any)
+})
 
 server.start(({ port }) =>
   console.log(`Server is running on http://localhost:${port}`),
